9d61d191f40af9d725610e10a7ae14f40d3f984c
muhammed saim <msaim366@gmail.com>
2026-01-15 12:20:08 +1100

    Add POST/ applications endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import request, jsonify
 from services import applications
 
 def register_routes(app):
@@ -6,4 +6,14 @@ def register_routes(app):
     @app.route("/applications", methods=["GET"])
     def get_applications():
         return jsonify(applications)
+    
+    
+    @app.route("/applications", methods = ["POST"])
+    def add_application():
+        data = request.get_json()
+        applications.append(data)
+        return jsonify({"message": "Application added"})
+
+    
+
     
--- app/run.py
+++ app/run.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 register_routes(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = 5001)
