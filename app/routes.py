fbf224080dc2573e434c6d94ce23fcddcb0e4c95
muhammed saim <msaim366@gmail.com>
2026-01-16 11:01:47 +1100

    Add validation to POST/ applications

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,9 @@ def register_routes(app):
     @app.route("/applications", methods = ["POST"])
     def add_application():
         data = request.get_json()
+        if  ("company_name" not in data or "role_title" not in data or "status" not in data):
+            return jsonify({"error":"Missing required fields"}), 400
+        
         applications.append(data)
         return jsonify({"message": "Application added"})
 
