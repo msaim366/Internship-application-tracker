be87ca1800a8d77673e0f3e5441cc180b867bf72
muhammed saim <msaim366@gmail.com>
2026-01-20 10:41:01 +1100

    Add Delete /applications/<index> endpoint

--- app/routes.py
+++ app/routes.py
@@ -12,18 +12,28 @@ def register_routes(app):
     def add_application():
         data = request.get_json()
         if  ("company_name" not in data or "role_title" not in data or "status" not in data):
-            return jsonify({"error":"Missing required fields"}), 400
+            return jsonify({"Error":"Missing required fields"}), 400
         
         applications.append(data)
         return jsonify({"message": "Application added"})
 
 
-    @app.route("/applications/<int:index>", methods=["GET"])
+    @app.route("/applications/<int:index>", methods =["GET"])
     def get_application(index):
         if index < 0 or index >= len(applications):
             return jsonify({"Error": "Application not found"}), 404
        
         return jsonify(applications[index])
+    
+    @app.route("/applications/<int:index>",methods = ["DELETE"])
+    def delete_application(index):
+        if index < 0 or index >= len(applications):
+            return jsonify({"Error": "Application not found"}), 404
+        
+        applications.pop(index)
+        return jsonify({"message": "Application deleted"})
+
+
 
     
 
