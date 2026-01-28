5b03345fb4f2cfa1e85472df7e616908445f1c22
muhammed saim <msaim366@gmail.com>
2026-01-28 12:14:35 +1100

    Add simple front-end to fetch and display applications

--- app/run.py
+++ app/run.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import register_routes
 
 app = Flask(__name__)
 register_routes(app)
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5001)
